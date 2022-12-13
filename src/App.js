b42a236c8e802fe8c16f4138c1a9278d55997175
Egor Ostryakov <eg.ostr@mail.ru>
2022-12-14 00:36:00 +0300

    oh shit, here we go again...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import {Heading, Text} from "@chakra-ui/react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Heading as="h1" size="lg" textAlign="center" letterSpacing={"tighter"}>
+                <Text>Oh shit, here we go again...</Text>
+            </Heading>
+        </div>
+    );
 }
 
 export default App;
